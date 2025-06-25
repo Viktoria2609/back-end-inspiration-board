d95e9de88b5789c6c8f907237075f515410968f5
Renata <murzinarenata@gmail.com>
2025-06-24 21:36:51 -0700

    Sync Alembic with remote DB: manual revision

---
+++ migrations/versions/afbf4ea68e60_manual_sync_with_db.py
@@ -0,0 +1,24 @@
+"""Manual sync with DB
+
+Revision ID: afbf4ea68e60
+Revises: 
+Create Date: 2025-06-24 21:36:17.235898
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'afbf4ea68e60'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
